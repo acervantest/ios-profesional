4e7ac02f9eac33ecccc585a6a9785c43eed65b4f
Alejandro <acervantest83@gmail.com>
2024-04-20 14:16:57 -0700

    Add bump frame up with computed value

--- ResetPassword/ResetPassword/ViewController.swift
+++ ResetPassword/ResetPassword/ViewController.swift
@@ -137,7 +137,26 @@ extension ViewController {
 // MARK: - Keyboard
 extension ViewController {
     @objc func keyboardWillShow(sender: NSNotification) {
-        view.frame.origin.y = view.frame.origin.y - 200
+        guard let userInfo = sender.userInfo,
+              let keyboardFrame = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue,
+              let currentTextField = UIResponder.currentFirst() as? UITextField else { return }
+        
+        //print("foo - userInfo: \(userInfo) - keyboardFrame: \(keyboardFrame) - currentTextField: \(currentTextField)")
+        
+        // check if the top of the keyboard is above the bottom of the currently focused textbox
+        let keyboardTopY = keyboardFrame.cgRectValue.origin.y
+        let convertedTextFieldFrame = view.convert(currentTextField.frame, from: currentTextField.superview)
+        let textFieldBottomY = convertedTextFieldFrame.origin.y + convertedTextFieldFrame.size.height
+
+        // if textField bottom is below keyboard bottom - bump the frame up
+        if textFieldBottomY > keyboardTopY {
+            let textBoxY = convertedTextFieldFrame.origin.y
+            let newFrameY = (textBoxY - keyboardTopY / 2) * -1
+            view.frame.origin.y = newFrameY
+            //print("foo - Adjust View!")
+        }
+
+        //print("foo - currentTextFieldFrame: \(currentTextField.frame) - convertedTextFieldFrame: \(convertedTextFieldFrame)")
     }
 
     @objc func keyboardWillHide(notification: NSNotification) {
